91f5046e6b390dca24b3eb88846b6fb2b82505dc
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2023-10-01 15:00:28 -0600

    Se agregaron 2 relative layouts (Favoritos y Calendar)

--- app/src/main/java/com/example/winestastic/MainActivity.java
+++ app/src/main/java/com/example/winestastic/MainActivity.java
@@ -14,7 +14,7 @@ import kotlin.jvm.functions.Function1;
 public class MainActivity extends AppCompatActivity {
 
     private MeowBottomNavigation bottomNavigation;
-    RelativeLayout settings, home, person;
+    RelativeLayout settings, person, home, favoritos, calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigation = findViewById(R.id.bottomNavigation);
         settings = findViewById(R.id.settings);
-        home = findViewById(R.id.home);
         person = findViewById(R.id.person);
+        home = findViewById(R.id.home);
+        favoritos = findViewById(R.id.favoritos);
+        calendar = findViewById(R.id.calendar);
 
 
 
-        bottomNavigation.show(2,true);
+
+        bottomNavigation.show(3,true);
 
 
         bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.baseline_settings_24));
-        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.baseline_home_24));
-        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.baseline_person_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.baseline_person_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.baseline_star_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(5, R.drawable.baseline_calendar_month_24));
 
 
         bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
@@ -46,24 +51,50 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
 
                         settings.setVisibility(View.VISIBLE);
-                        home.setVisibility(View.GONE);
                         person.setVisibility(View.GONE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
 
                         break;
 
                     case 2:
 
                         settings.setVisibility(View.GONE);
-                        home.setVisibility(View.VISIBLE);
-                        person.setVisibility(View.GONE);
+                        person.setVisibility(View.VISIBLE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
 
                         break;
 
                     case 3:
 
                         settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
+                        home.setVisibility(View.VISIBLE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
+
+                        break;
+
+                    case 4:
+
+                        settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
                         home.setVisibility(View.GONE);
-                        person.setVisibility(View.VISIBLE);
+                        favoritos.setVisibility(View.VISIBLE);
+                        calendar.setVisibility(View.GONE);
+
+                        break;
+
+                    case 5:
+
+                        settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.VISIBLE);
 
                         break;
 
@@ -82,8 +113,10 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
 
                         settings.setVisibility(View.VISIBLE);
-                        home.setVisibility(View.GONE);
                         person.setVisibility(View.GONE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
 
                         break;
                 }
@@ -102,8 +135,10 @@ public class MainActivity extends AppCompatActivity {
                     case 2:
 
                         settings.setVisibility(View.GONE);
-                        home.setVisibility(View.VISIBLE);
-                        person.setVisibility(View.GONE);
+                        person.setVisibility(View.VISIBLE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
 
                         break;
                 }
@@ -122,8 +157,54 @@ public class MainActivity extends AppCompatActivity {
                     case 3:
 
                         settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
+                        home.setVisibility(View.VISIBLE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.GONE);
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+
+                switch (model.getId()){
+
+                    case 4:
+
+                        settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
                         home.setVisibility(View.GONE);
-                        person.setVisibility(View.VISIBLE);
+                        favoritos.setVisibility(View.VISIBLE);
+                        calendar.setVisibility(View.GONE);
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+
+                switch (model.getId()){
+
+                    case 5:
+
+                        settings.setVisibility(View.GONE);
+                        person.setVisibility(View.GONE);
+                        home.setVisibility(View.GONE);
+                        favoritos.setVisibility(View.GONE);
+                        calendar.setVisibility(View.VISIBLE);
 
                         break;
                 }
